259882a34ce29c70cdb205ebbff1b76dab2b7623
Undery <127064386+Undery33@users.noreply.github.com>
2023-04-03 11:20:21 +0900

    Add files via upload

---
+++ chap03_ex01.sql
@@ -0,0 +1,34 @@
+/* 도서 번호가 1인 도서의 이름 */
+SELECT bookname
+FROM book
+WHERE bookid = 1;
+
+/* 가격이 20,000원 이상인 도서의 이름 */
+SELECT bookname
+FROM book
+WHERE price >= 20000;
+
+/* 박지성의 총 구매액 */
+SELECT SUM(saleprice)
+FROM orders
+WHERE custid = 1;
+
+/* 박지성의 총 구매액(2)*/
+SELECT SUM(saleprice)
+FROM Customer, orders
+WHERE Customer.custid = Orders.custid AND Customer.name LIKE '박지성' ;
+
+/* 박지성이 구매한 도서의 수 */
+SELECT COUNT(*)
+FROM customer, orders
+WHERE customer.custid = orders.custid AND customer.name LIKE '박지성';
+
+/* 박지성이 구매한 도서의 출판사 수 */
+SELECT COUNT(publisher)
+FROM book, customer, orders
+WHERE book.bookid = orders.bookid AND customer.custid = orders.custid AND customer.name LIKE '박지성';
+
+/* 박지성이 구매한 도서의 이름, 가격, 정가와 판매 가격의 차이 */
+
+
+/* 박지성이 구매하지 않은 도서의 이름 */
