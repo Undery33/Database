42d4e4f17e28a85d574940932dd32dcb5bb9f3dc
Undery <127064386+Undery33@users.noreply.github.com>
2023-04-11 11:14:42 +0900

    Rename 0404ch03.sql to 연습문제/0404ch03.sql

--- 연습문제/0404ch03.sql
+++ 연습문제/0404ch03.sql
@@ -1,30 +1,30 @@
-/* ÁúÀÇ 3-23 */
+/* ì§ˆì˜ 3-23 */
 SELECT name, saleprice
 FROM customer, orders
 WHERE customer.custid = orders.custid;
 
-/* ÁúÀÇ 3-27 */
+/* ì§ˆì˜ 3-27 */
 SELECT name, saleprice
 FROM customer LEFT OUTER JOIN orders
 ON customer.custid=orders.custid;
 
-/* (+)¸¦ ÀÌ¿ëÇÑ ¿ÞÂÊ ¿ÜºÎ Á¶ÀÎ */
+/* (+)ë¥¼ ì´ìš©í•œ ì™¼ìª½ ì™¸ë¶€ ì¡°ì¸ */
 SELECT customer.name, saleprice
 FROM customer, orders
 WHERE customer.custid = orders.custid(+);
 
-/* ÁúÀÇ 3-28 */
+/* ì§ˆì˜ 3-28 */
 SELECT bookname
 FROM book
 WHERE price=(SELECT MAX(price) FROM book);
 
-/* ÁúÀÇ 3-29 */
+/* ì§ˆì˜ 3-29 */
 SELECT name
 FROM customer
 WHERE custid IN(SELECT custid
                 FROM orders);
                 
-/* ÁúÀÇ 3-31 */
+/* ì§ˆì˜ 3-31 */
 SELECT b1.bookname
 FROM book b1
 WHERE b1.price>(select avg(b2.price)
